00166b8c7be77b8ffb6e9cc133ba1703f7ef5940
owner <20175454+zombietan@users.noreply.github.com>
2025-05-29 21:46:36 +0900

    add libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.parcelize)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.jetbrains.kotlin.serialization)
 }
 
 android {
@@ -56,4 +60,49 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.compose.icons)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.androidx.compose.navigation)
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.kotlinx.serialization.core)
+    implementation(libs.androidx.datastore.preferences)
+
+    // compose lifecycle
+    implementation(libs.compose.lifecycle)
+
+    // compose viewmodel
+    implementation(libs.compose.viewmodel)
+
+    // compose hilt
+    implementation(libs.compose.hilt)
+
+    // compose paging
+    implementation(libs.compose.paging)
+
+    // splash
+    implementation(libs.splash)
+
+    // collections
+    implementation(libs.collections)
+
+    // firebase
+
+    // hilt
+    implementation(libs.hilt)
+    ksp(libs.hilt.compiler)
+
+    // coroutines
+    implementation(libs.coroutines)
+    implementation(libs.coroutines.android)
+
+    // room
+    implementation(libs.room)
+    implementation(libs.room.ktx)
+    implementation(libs.room.paging)
+    ksp(libs.room.compiler)
+
+
+    // timber
+    implementation(libs.timber)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,8 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.parcelize) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.jetbrains.kotlin.serialization) apply false
 }
