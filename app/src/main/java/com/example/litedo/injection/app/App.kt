b720793f78942e2d23c6d1e680e465e8e61ace07
owner <20175454+zombietan@users.noreply.github.com>
2025-05-30 00:46:03 +0900

    add Timber

--- app/src/main/java/com/example/litedo/injection/app/App.kt
+++ app/src/main/java/com/example/litedo/injection/app/App.kt
@@ -1,11 +1,21 @@
 package com.example.litedo.injection.app
 
 import android.app.Application
+import com.example.litedo.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import javax.inject.Inject
+
 
 @HiltAndroidApp
 class App : Application() {
+
+    @Inject
+    lateinit var tree: Timber.Tree
+
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) Timber.plant(tree)
     }
 }
