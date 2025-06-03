5f037e548b8287ca9b58fd7c566312418b1b4e78
owner <20175454+zombietan@users.noreply.github.com>
2025-06-03 22:42:05 +0900

    refactor: add annotation

--- app/src/main/java/com/example/litedo/presentation/component/topbar/TopBarIcon.kt
+++ app/src/main/java/com/example/litedo/presentation/component/topbar/TopBarIcon.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
+@Suppress("unused")
 @Composable
 fun TopBarIcon(
     @DrawableRes icon: Int,
